720a76bfe354365aa70fd4096b0a460430afe138
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-05-22 15:11:54 +0300

    feat: add homework8

--- homework7.py
+++ homework7.py
@@ -16,7 +16,6 @@ def guess_number():
             elif answer == 'n':
                 print('Good bye!')
                 break
-
         elif user_num < num:
             print(f"Try again. The hidden number is greater than {user_num}")
         elif user_num > num:
@@ -24,3 +23,35 @@ def guess_number():
 
 
 guess_number()
+
+# coach's solution
+# import random
+#
+# x = random.randint(1, 100)
+# user_num = 0
+# cnt = 0
+#
+# while True:
+#     user_num = int(input('Я загадал число от 1 до 100 - угадай его: '))
+#     cnt += 1
+#     if user_num == x:
+#         print(f'Ты угадал число за {cnt} попыток')
+#         print("""
+#                 ------------
+#                 |           |
+#                 |  0     0  |
+#                 |     <     |
+#                 |  .     .  |
+#                 |   `...`   |
+#                 ------------
+#                 """)
+#         if input('Сыграем еще? "y|n":') == 'y':
+#             x = random.randint(1, 100)
+#             cnt = 0
+#         else:
+#             print('Спасибо за игру')
+#             break
+#     elif user_num > x:
+#         print('Мое число меньше')
+#     else:
+#         print('Мое число больше')
