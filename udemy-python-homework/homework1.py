cd4c9975e0d48e6184ae218009d703072f9e648a
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-06-21 15:02:35 +0300

    feat: add udemy-python-homework

---
+++ udemy-python-homework/homework1.py
@@ -0,0 +1,10 @@
+print('Welcome to the Band Name Generator.')
+name_city = input("Enter name of city when you grew up: ")
+name_pet = input("Enter name of pet:")
+print(name_city + " " + name_pet)
+
+# Coach's solution
+# print("Welcome to the Band Name Generator.")
+# street = input("What's name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + street + " " + pet)
